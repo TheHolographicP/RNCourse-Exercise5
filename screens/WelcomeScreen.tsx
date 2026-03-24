2ab16c9699259948478ce75acc55cdb73341231d
TheHolographicP <thegamblerkid@gmail.com>
2026-03-24 10:51:06 -0600

    Implement authenticated data fetch from firebase;

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,9 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { useContext, useEffect, useState} from 'react';
+import axios from 'axios';
+import { AuthContext } from 'store/auth-context';
+
 
 export function WelcomeScreen() {
+  const [message, setMessage] = useState('');
+  const authContext = useContext(AuthContext);
+  const token = authContext.token;
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `https://react-native-course-7fe21-default-rtdb.firebaseio.com/message.json?auth=${token}`);
+        console.log('Fetched data:', response.data);
+        setMessage(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
+      <Text>{message}</Text>
       <Text>You authenticated successfully!</Text>
     </View>
   );
